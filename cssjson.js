5b980c5911c07b8cb973dc052ff3391ca82a318c
dotvhs <voythas@gmail.com>
2019-09-12 03:12:50 +0200

    Initial version.

---
+++ cssjson.js
@@ -0,0 +1,168 @@
+/**
+ * CSS-JSON Converter for JavaScript
+ * Converts CSS to JSON and back.
+ * Version 2.1
+ *
+ * Released under the MIT license.
+ *
+ * Copyright (c) 2013 Aram Kocharyan, http://aramk.com/
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+var CSSJSON = new function() {
+    var base = this;
+
+    base.init = function() {
+        // String functions
+        String.prototype.trim = function() {
+            return this.replace(/^\s+|\s+$/g, "");
+        };
+
+        String.prototype.repeat = function(n) {
+            return new Array(1 + n).join(this);
+        };
+    };
+    base.init();
+
+    var commentX = /\/\*[\s\S]*?\*\//g;
+    var lineAttrX = /([^\:]+):([^\;]*);/;
+
+    // This is used, a concatenation of all above. We use alternation to
+    // capture.
+    var altX = /(\/\*[\s\S]*?\*\/)|([^\s\;\{\}][^\;\{\}]*(?=\{))|(\})|([^\;\{\}]+\;(?!\s*\*\/))/gim;
+
+    // Capture groups
+    var capComment = 1;
+    var capSelector = 2;
+    var capEnd = 3;
+    var capAttr = 4;
+
+    var isEmpty = function(x) {
+        return typeof x == "undefined" || x.length == 0 || x == null;
+    };
+
+    var isCssJson = function(node) {
+        return !isEmpty(node) ? node : false;
+    };
+
+    /**
+     * Input is css string and current pos, returns JSON object
+     *
+     * @param cssString
+     *            The CSS string.
+     * @param args
+     *            An optional argument object. ordered: Whether order of
+     *            comments and other nodes should be kept in the output. This
+     *            will return an object where all the keys are numbers and the
+     *            values are objects containing "name" and "value" keys for each
+     *            node. comments: Whether to capture comments. split: Whether to
+     *            split each comma separated list of selectors.
+     */
+    base.toJSON = function(cssString, args) {
+        var node = {};
+        var match = null;
+        var count = 0;
+
+        if (typeof args == "undefined") {
+            var args = {
+                ordered: false,
+                comments: false,
+                stripComments: false,
+                split: false
+            };
+        }
+        if (args.stripComments) {
+            args.comments = false;
+            cssString = cssString.replace(commentX, "");
+        }
+
+        while ((match = altX.exec(cssString)) != null) {
+            if (!isEmpty(match[capComment]) && args.comments) {
+                // Comment
+                var add = match[capComment].trim();
+                node[count++] = add;
+            } else if (!isEmpty(match[capSelector])) {
+                // New node, we recurse
+                var name = match[capSelector].trim();
+                // This will return when we encounter a closing brace
+                var newNode = base.toJSON(cssString, args);
+                if (args.ordered) {
+                    var obj = {};
+                    obj["name"] = name;
+                    obj["value"] = newNode;
+                    // Since we must use key as index to keep order and not
+                    // name, this will differentiate between a Rule Node and an
+                    // Attribute, since both contain a name and value pair.
+                    obj["type"] = "rule";
+                    node[count++] = obj;
+                } else {
+                    if (args.split) {
+                        var bits = name.split(",");
+                    } else {
+                        var bits = [name];
+                    }
+                    for (var i = 0; i < bits.length; i++) {
+                        var sel = bits[i].trim();
+                        if (sel in node) {
+                            for (var att in newNode) {
+                                node[sel][att] = newNode[att];
+                            }
+                        } else {
+                            node[sel] = newNode;
+                        }
+                    }
+                }
+            } else if (!isEmpty(match[capEnd])) {
+                // Node has finished
+                return node;
+            } else if (!isEmpty(match[capAttr])) {
+                var line = match[capAttr].trim();
+                var attr = lineAttrX.exec(line);
+                if (attr) {
+                    // Attribute
+                    var name = attr[1].trim();
+                    var value = attr[2].trim();
+                    if (args.ordered) {
+                        var obj = {};
+                        obj["name"] = name;
+                        obj["value"] = value;
+                        obj["type"] = "attr";
+                        node[count++] = obj;
+                    } else {
+                        if (name in node) {
+                            var currVal = node[name];
+                            if (!(currVal instanceof Array)) {
+                                node[name] = [currVal];
+                            }
+                            node[name].push(value);
+                        } else {
+                            node[name] = value;
+                        }
+                    }
+                } else {
+                    // Semicolon terminated line
+                    node[count++] = line;
+                }
+            }
+        }
+
+        return node;
+    };
+}();
+
+if (typeof module === "object") {
+    module.exports = CSSJSON;
+}
